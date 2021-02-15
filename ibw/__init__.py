4b982698e6c90c1eed92b1380e5be5dc2c26c101
Ruil <ult.rui.liu@gmail.com>
2021-02-15 16:11:15 -0500

    refactor

--- ibw/__init__.py
+++ ibw/__init__.py
@@ -1,4 +1,4 @@
+from . import client_base # noqa: F401
 from . import authorization  # noqa: F401
 from . import accounts  # noqa: F401
 from . import order  # noqa: F401
-from . import client_base # noqa: F401
