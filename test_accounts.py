d5d2e10afadaa8a3441510d68c98dc2478d5a5d2
Ruil <ult.rui.liu@gmail.com>
2021-07-17 17:10:46 -0400

    update tests

--- test_accounts.py
+++ test_accounts.py
@@ -7,5 +7,7 @@ print('portfolio_accounts: ', my_accounts.portfolio_accounts())
 
 print('Position is: ', my_accounts.portfolio_positions('73340487'))
 
-print('account summary is: ', my_accounts.portfolio_account_summary('U5531042'))
+summary = my_accounts.portfolio_account_summary('U5531042')
+print('account summary is: ', summary)
 
+print('balance is: ', summary['totalcashvalue']['amount'])
