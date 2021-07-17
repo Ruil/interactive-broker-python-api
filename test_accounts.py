81039ac7f7c6e67c383ffc6261cc73c15c552a50
Ruil <ult.rui.liu@gmail.com>
2021-07-17 17:00:21 -0400

    authorization must input enter

--- test_accounts.py
+++ test_accounts.py
@@ -7,4 +7,5 @@ print('portfolio_accounts: ', my_accounts.portfolio_accounts())
 
 print('Position is: ', my_accounts.portfolio_positions('73340487'))
 
+print('account summary is: ', my_accounts.portfolio_account_summary('U5531042'))
 
